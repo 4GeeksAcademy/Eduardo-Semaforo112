8da24394698f750afe0ae85f35fa82f07a05f2d0
Incerteza88 <info@incerteza.es>
2025-06-13 11:03:03 +0000

    semaforo terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,19 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+import Semaforo from "./Semaforo";
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+//create your first component
+function Home() {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-4">Mi Semáforo</h1>
+      <Semaforo />
+    </div>
+  );
+}
 
 export default Home;
